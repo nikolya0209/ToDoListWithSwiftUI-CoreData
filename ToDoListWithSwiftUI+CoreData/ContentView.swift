046daa3f2bc579b5c478e7c9872367e09bf92319
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-02-20 16:56:05 +0300

    UI

--- ToDoListWithSwiftUI+CoreData/ContentView.swift
+++ ToDoListWithSwiftUI+CoreData/ContentView.swift
@@ -10,14 +10,66 @@ import SwiftUI
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(entity: Order.entity(),
+                  sortDescriptors: [
+        NSSortDescriptor(keyPath: \Order.drink,
+                         ascending: true)
+    ]) var orders: FetchedResults<Order>
+    
     @State private var newOrder = ""
     
     var body: some View {
-        List {
-            Section(header: Text("NewOrder")) {
-                HStack {
-                    TextField("New order", text: $newOrder)
+        NavigationView {
+            List {
+                Section(header: Text("NewOrder")) {
+                    HStack {
+                        TextField("New order", text: $newOrder)
+                        Button(action: {
+                            let order = Order(context: self.managedObjectContext)
+                            order.drink = self.newOrder
+                            order.createdAt = Date()
+                            do {
+                                try self.managedObjectContext.save()
+                            } catch {
+                                print(error)
+                            }
+                            self.newOrder = ""
+                        }, label: {
+                            Image(systemName: "plus.circle.fill")
+                        })
+                    }
                 }
+                Section(header: Text("Your Orders")) {
+                    ForEach(self.orders, id: \.self) { order in
+                        OderItemView(drink: order.drink ?? "choose drink", createdAt: "\(String(describing: order.createdAt))")
+                    }.onDelete(perform: removeOrder)
+                }
+            }.navigationBarTitle(Text("Order View"))
+            .navigationBarItems(trailing: EditButton())
+        }
+    }
+    func removeOrder(at offSets: IndexSet) {
+        for index in offSets {
+            let man = orders[index]
+            managedObjectContext.delete(man)
+            do {
+                try self.managedObjectContext.save()
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
+struct OderItemView: View {
+    var drink: String = ""
+    var createdAt: String = ""
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(drink).font(.headline)
+                Text(createdAt).font(.caption)
             }
         }
     }
